b0c32f48a5d200921d23a901ea17ed7b6a611eb2
MWolff89 <150514371+MWolff89@users.noreply.github.com>
2024-02-27 04:31:10 +0000

    removed unnnecessaryh distracing parts

--- main.py
+++ main.py
@@ -36,40 +36,6 @@ async def shutdown_event():
 app.add_event_handler("startup", startup_event)
 app.add_event_handler("shutdown", shutdown_event)
 
-# while inpute does not equal "exit":
-#     inpute = input("Enter your query: ")
-#     result = agent.chat(inpute)
-#     print(result)
-
-# thread_id = "xxx"
-# if thread_id not in tesa_memory_buffers:
-#     tesa_memory_buffers[thread_id] = ChatMemoryBuffer(
-#         token_limit=128000, max_tokens=128000, max_turns=20
-#     )
-
-# user_memory = tesa_memory_buffers[thread_id]
-
-# while True:
-#     inpute = input("Enter your query: ")
-#     agent = OpenAIAgent.from_tools(tesa_tools, llm=llm, verbose=True, system_prompt=tesa_system_prompt, memory=user_memory)
-#     result = agent.chat(inpute)
-#     print(result)
-
-# @app.post("/query")
-# async def query(query: Query):
-#     if query.thread_id not in user_memory_buffers:
-#         user_memory_buffers[query.thread_id] = ChatMemoryBuffer(token_limit=1000, max_tokens=1000, max_turns=10)
-    
-#     user_memory = user_memory_buffers[query.thread_id]
-#     agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=base_context, memory=user_memory)
-    
-#     result = agent.chat(query.text)
-#     print(f"Thread: {query.thread_id}, Query: {query.text}, Result: {result}")
-    
-#     return {
-#         "response": result
-#     }
-
 @app.post("/query")
 async def tesa_query(query: Query):
     if query.thread_id not in tesa_memory_buffers:
